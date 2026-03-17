7072b1700eaff99b7f47517f1e7c025f529809a2
Dlomka <79089800+Dlomka@users.noreply.github.com>
2026-03-17 15:49:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const rooms = {};
 
 io.on('connection', (socket) => {
